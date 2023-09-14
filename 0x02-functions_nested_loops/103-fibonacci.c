73cb18f0e0e7ea0cb39d90d55bedc741820084f4
Mwasbuits <kabuitueric5@gmail.com>
2023-09-13 17:57:15 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description-Fibonacci
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	long int fibonacci[50], sum = 2;
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+
+	for (a = 2; a < 50; a++)
+	{
+		fibonacci[a] = fibonacci[a - 1] + fibonacci[a - 2];
+		if ((fibonacci[a] % 2) == 0 && fibonacci[a] < 4000000)
+			sum += fibonacci[a];
+	}
+	printf("%ld\n", sum);
+
+	return (0);
+}
